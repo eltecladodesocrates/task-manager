9ab6d8330e9b6d905c16711bf731d89b1c3b85a9
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-01-20 17:01:54 -0500

    Use mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,21 +1,49 @@
 // CRUD: Create Read Update Delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 
     if(error) {
        return console.log('Unable to connect to database')
     }
-    const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'Athos',
-        age: 29
-    })
 
+    const db = client.db(databaseName)
+
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("6008297dfa9bf00b2b910fea")
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // db.collection('tasks').updateMany({
+    //     completed: true
+    // }, {
+    //     $set: {
+    //         completed: false
+    //     }
+    // }).then((results) => {
+    //     console.log(results.modifiedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('tasks').deleteMany({
+        description: 'Read'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 })
 
