5402dd351f3d633c2120c61749c159e88f918c1d
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-01-21 18:31:54 -0500

    Add models and express

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,22 +5,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    } 
-})
 
-const me = new User({
-    name: 'Chicot',
-    age: 25
-})
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log(error)
-})
