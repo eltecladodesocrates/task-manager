b526e2be34f22400d128a456813281c745669b78
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-02-15 10:39:30 -0500

    auth users

--- src/index.js
+++ src/index.js
@@ -6,6 +6,28 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+const multer = require('multer')
+const upload = multer({ 
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+    
+        if(!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+        cb(undefined, true)
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,3 +35,60 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
+/////////////////////////////////////////
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async () => {
+
+//     const token = jwt.sign({ _id: "abc123" }, 'thisismynewcourse', { expiresIn: '7 days' })
+//     console.log(token)
+
+//     const data = jwt.verify(token, 'thisismynewcourse')
+//     console.log(data)
+
+// }
+
+// myFunction()
+
+// const pet = {
+//     name: 'Lopez'
+// }
+
+// pet.toJSON = function () {
+//     return {}
+// }
+
+// console.log(JSON.stringify(pet))
+
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+// const taskToUser = async () => {
+//     const task = await Task.findById('601c973ee49d866ff50dd5b2')
+//     // This is going to find the user related to this task
+//     await task.populate('owner').execPopulate()
+//     console.log(task)
+// }
+
+// const userToTask = async () => {
+//     const user = await User.findById('601c96193059a16fa9bac39f')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
+
+// userToTask()
+
+// users
+
+// {
+//     "email": "athosmosqetero@example.com",
+//     "password": "bragelone"
+// }
+
+// {
+//     "email": "aramismosquetero@example.com",
+//     "password": "fouquete"
+// }
+
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
+
+const upload = multer({ 
+    dest: 'avatar'
+})
 
 router.post('/users', async (req, res) => {
 
     const user = new User(req.body)
+    const token = await user.generateAuthToken()
+    res.send( { user, token})
     try {
         await user.save()
         res.status(201).send(user)
@@ -14,33 +22,47 @@ router.post('/users', async (req, res) => {
 
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token})
+    } catch (e) {
+        res.status(400).send
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
 
     try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        res.status(500).send(e)
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+        
+    } catch (e) {
+        res.status(500).send()
     }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+
+    res.send(req.user)
 
 })
 
-router.get('/users/:id', async (req, res) => {
-    
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch(e) {
-        res.status(400).send()
-    }
-
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     
     const updates = Object.keys(req.body)
     const allowUpdates = ['name', 'email', 'password', 'age']
@@ -50,27 +72,26 @@ router.patch('/users/:id', async (req, res) => {
         return res.status(400).send({ error: 'Invalida updates' })
     }
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if(!user) {
-            return res.status(404).send('No user found')
-        }
-        res.send(user)
+        updates.forEach((update) =>  req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
 
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
 })
 
 module.exports = router
